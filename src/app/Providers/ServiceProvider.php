ea0c430c4b7a9908626a511bd8c98b8d487006f5
Monur <monurakkaya@gmail.com>
2018-08-19 05:24:01 +0300

    translation publish

--- src/app/Providers/ServiceProvider.php
+++ src/app/Providers/ServiceProvider.php
@@ -15,11 +15,11 @@ class ServiceProvider extends BaseProvider
 {
     public function boot()
     {
-        $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'laravel-alert');
         $this->loadViewsFrom(__DIR__.'/../../resources/views/laravel-alert', 'laravel-alert');
         $this->publishes([
            __DIR__.'/../../public' => public_path('laravel-alert/'),
-            __DIR__.'/../../config/laravel-alert.php' => config_path('laravel-alert.php')
+            __DIR__.'/../../config/laravel-alert.php' => config_path('laravel-alert.php'),
+            __DIR__.'/../../resources/lang' => resource_path('lang')
         ], 'laravel-alert');
     }
 }
